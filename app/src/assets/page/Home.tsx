8479a738775b897d3b968feb6662d5848fbf7cd0
DaneVN <Dane.vN23@gmail.com>
2026-08-07 15:35:30 +0200

    Boilerplate cleanup + React Router DOM Added

---
+++ app/src/assets/page/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <>
+      <h1 className="text-blue-500 font-bold">Home Page</h1>
+      <p>Welcome to the Home Page!</p>
+    </>
+  );
+}
+
+export default Home;
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+/**
+ * The entry point of the React application. It renders the App component inside a StrictMode and BrowserRouter for routing.
+ * @returns
+ */
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App classN="flex flex-col" />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
