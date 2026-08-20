7e5190599da56008f693dbed4d9c02ba2fedd334
DaneVN <Dane.vN23@gmail.com>
2026-08-20 16:51:39 +0200

    Cleanup Header and nav doubles

--- app/src/assets/pages/Home.tsx
+++ app/src/assets/pages/Home.tsx
@@ -1,14 +1,9 @@
 import AppCard from "../components/AppCard";
 import Card from "../components/Card";
-import Header from "../components/Header";
-import Navbar from "../components/Navbar";
 
 function Home() {
   return (
     <>
-      <Header />
-      <Navbar />
-      <br />
       <main className="flex flex-col items-center justify-between p-4">
         <p className="text-lg text-(--cl-dark-blue) mb-4">
           This is the home page of the RE/MAX Unity Reboot application.
